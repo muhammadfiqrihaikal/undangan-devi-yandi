e4bd545b5ca035f5cb8a64732055a8b55b895b9d
muhammadfiqrihaikal <muhammadfiqrih3@gmail.com>
2026-05-05 22:59:36 +0700

    Membuat file robots.ts untuk nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,30 @@ const raleway = Raleway({
 
 export const metadata: Metadata = {
   title: "Undangan Pernikahan Devi & Yandi",
-  description: "Kami mengundang Anda untuk merayakan pernikahan Devi Apriliyanti Safara & Yandi Hidayat pada 24 Mei 2026.",
+  description:
+    "Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.",
+  openGraph: {
+    title: "Undangan Pernikahan Devi & Yandi",
+    description:
+      "Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.",
+    url: "https://undangan-devi-yandi.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://undangan-devi-yandi.vercel.app/asset/cover-utama.png",
+        width: 1200,
+        height: 630,
+        alt: "Undangan Pernikahan Devi & Yandi",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Undangan Pernikahan Devi & Yandi",
+    description:
+      "Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.",
+    images: ["https://undangan-devi-yandi.vercel.app/asset/cover-utama.png"],
+  },
 };
 
 export default function RootLayout({
